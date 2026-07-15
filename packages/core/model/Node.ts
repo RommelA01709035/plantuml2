40ccbaa4dfe23d4c3103e132182d546a34c6d0c2
July230 <A01352823@tec.mx>
2026-07-14 19:30:26 -0600

    add: Agregar clase base Node

--- packages/core/model/Node.ts
+++ packages/core/model/Node.ts
@@ -1,6 +1,26 @@
+import { Point, Size } from '../geometry';
+
+/**
+ * Represents a node in a diagram with an identifier, name, kind, position, and size.
+ * 
+ * @remarks
+ * This class is part of the core model module and can be used to define nodes in a diagram.
+ * 
+ * @public
+ */
 export class Node {
     readonly id: string;
     name: string;
+    label?: string;
+    kind: string;
     position: Point;
     size: Size;
+
+    constructor(id: string, name: string, kind: string = "node") {
+        this.id = id;
+        this.name = name;
+        this.kind = kind;
+        this.position = new Point();
+        this.size = new Size();
+    }
 }
