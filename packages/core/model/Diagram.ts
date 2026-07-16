96819b932470212b3126269573f5ad4206ad5147
July230 <A01352823@tec.mx>
2026-07-15 19:14:16 -0600

    add: Agregar edge y label en diagram

--- packages/core/model/Diagram.ts
+++ packages/core/model/Diagram.ts
@@ -1,4 +1,6 @@
 import { Node } from "./Node";
+import { Edge } from "./Edge";
+import { Label } from "./Label";
 
 /**
  * Represents a complete diagram.
@@ -13,11 +15,15 @@ export class Diagram {
     readonly id: string;
     name: string;
     private readonly _nodes: Node[];
+    private readonly _edges: Edge[];
+    private readonly _labels: Label[];
 
     constructor(id: string, name: string = "Untitled diagram"){
         this.id = id;
         this.name = name;
         this._nodes = [];
+        this._edges = [];
+        this._labels = [];
     } 
 
     /**
@@ -53,5 +59,84 @@ export class Diagram {
      */
     getNodes(): Node[] {
         return this._nodes;
+    }
+
+    /**
+     * Adds an edge to the diagram
+     * 
+     * @param edge - The edge to be added to the diagram.
+     */
+    addEdge(edge: Edge): void {
+        this._edges.push(edge);
+    }
+
+    /**
+     * Removes an edge from the diagram
+     * @param id - The identifier of the edge to be removed.
+     * @returns True if the edge was removed, false otherwise.
+     */
+    removeEdge(id: string): boolean {
+        const index = this._edges.findIndex(edge => edge.id === id);
+        if(index === -1){
+            return false;
+        }
+        this._edges.splice(index, 1);
+        return true;
+    }
+
+    /**
+     * Returns an edge by its identifier
+     * @param id - The identifier of the edge to be retrieved.
+     * @returns The edge if found, undefined otherwise.
+     */
+    getEdge(id: string): Edge | undefined {
+        return this._edges.find(edge => edge.id === id);
+    }
+
+    /**
+     * Returns all edges in the diagram.
+     * @returns The array of edges.
+     */
+    getEdges(): Edge[] {
+        return this._edges;
+    }
+
+    /**
+     * Adds a label to the diagram.
+     * @param label - The label to be added to the diagram.
+     */
+    addLabel(label: Label): void {
+        this._labels.push(label);
+    }
+
+    /**
+     * Removes a label from the diagram.
+     * @param id - The identifier of the label to be removed.
+     * @returns True if the label was removed, false otherwise.
+     */
+    removeLabel(id: string): boolean {
+        const index = this._labels.findIndex(label => label.id === id);
+        if(index === -1){
+            return false;
+        }
+        this._labels.splice(index, 1);
+        return true;
+    }
+
+    /**
+     * Returns a label by its identifier.
+     * @param id - The identifier of the label to be retrieved.
+     * @returns The label if found, undefined otherwise.
+     */
+    getLabel(id: string): Label | undefined {
+        return this._labels.find(label => label.id === id);
+    }
+
+    /**
+     * Returns all labels in the diagram.
+     * @returns The array of labels.
+     */
+    getLabels(): Label[] {
+        return this._labels;
     }
 }
